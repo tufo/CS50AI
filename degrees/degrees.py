5cdc3fa3810207915af28accd91edc6f2cb1bff1
tufo <cal.tufo@gmail.com>
2024-11-07 19:55:34 -0500

    modified degrees project

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -70,7 +70,7 @@ def main():
         sys.exit("Person not found.")
 
     path = shortest_path(source, target)
-
+    
     if path is None:
         print("Not connected.")
     else:
@@ -92,8 +92,114 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
-    raise NotImplementedError
+    # TO DO
+    # raise NotImplementedError
+
+    #self.source = source
+    #self.goal = goal
+    
+    # initialize variable to keep track of number of states explored.
+    #self.states_explored = 0
+    states_explored = 0
+
+    # create node that represents the start state (position, source).
+    origin = Node(state=source, parent=None, action=None)
+
+    # create a frontier (choose stack or queue)
+    frontier1 = QueueFrontier() # notice there is no need to enter anything within parentheses.
+
+    # add start state to frontier.
+    frontier1.add(origin)
+
+    # initialize explored set within self, which is empty
+    # remember that sets are unordered and all elements are intended to be interpreted together as a unit, not individually/separately.
+    #self.explored_set = set()
+    explored_set = set()
+
+    # implement loop.
+    while True:
+
+        # check whether the entire frontier is empty; call the .empty function.
+        if frontier1.empty() == True:
+            return "There is no solution."
+
+        # call the .remove function, which removes node from frontier, and also assigns that selected node to be the active node.
+        current_node = frontier1.remove()
+
+        # update the number of states explored.
+        states_explored += 1
+
+        # check if state of node is the goal; if yes, then...
+        if current_node.state == target:
+            # initialize backtrack arrays (list variables).
+            print("*************** PROBLEM HAS BEEN SOLVED ***************")
+            path = []
+
+            # target (goal), self, parents are states.
+            # STATE: current person
+            # PARENT: previous person
+            # ACTION: a connector line, coordinates: movie_id
+
+            # WHILE there are still parents to look for...
+            while current_node.parent != None:
+            
+                # keep following up the parent nodes to find solution.
+                # 42:18 in lecture
+                
+                # append (action, state) 2-tuple to path
+                entry = (current_node.action, current_node.state)
+                path.append(entry)
+
+                # reassign the current_node to be its parent for next iteration.
+                current_node = current_node.parent
+
+            path.reverse()
+
+            """
+            print("Actions:")
+            print(actions)
+            print("States:")
+            print(states)               
+            print("Path: ")
+            print(path)
+            """
+
+            return path
+            
+        # if goal has not yet been found, then...
+        # add the state to the explored_set
+        explored_set.add(current_node.state)
+
+        # Add neighbors to the frontier using neighbors_for_person()
+        # print(neighbors) will return: [('movie_id', 'person_id'), ...]
+        # 43:18
+
+        neighbors = neighbors_for_person(current_node.state) # this is a list of 2-tuples.
+
+        """
+        # Kevin Bacon: 102
+        # Demi Moore: 193
+        # A Few Good Men: 104257
+
+        print("person_id: ")
+        print(current_node.state)
+        print("Neighbors: ")
+        print(neighbors)
+        """
+
+        # self.neighbors will return a 2-tuple of (action, state)
+        # for loop: (action, state) is a 2-tuple corresponding to 
+        # for (action, state) in [(action1, state1), (action2, state2), ...]
+        for (action, state) in (neighbors):
+            # the state element has been extracted via the for loop.
+
+            # exclude 'current_node by checking that it's not in the frontier' & 'already-explored states'
+            if not frontier1.contains_state(state) and state not in explored_set:
+                # create a child node; assign values of action and state to the object attributes.
+                child = Node(state=state, parent=current_node, action=action)
+                # add that new child node to the frontier.
+                frontier1.add(child)
+
 
 
 def person_id_for_name(name):
